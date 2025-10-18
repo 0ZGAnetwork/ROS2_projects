c350758d74137c6a5adc6960992b0a4d3a75814a
0ZGA <ozgaapawell@gmail.com>
2025-10-18 21:25:27 +0200

    add  ability to set parameters during runtime for generator.py and scaler.py

--- src/my_package/my_package/generator.py
+++ src/my_package/my_package/generator.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 import math
 import time
+from rcl_interfaces.msg import SetParametersResult
 
 class GenSinus(Node):
     def __init__(self):
@@ -13,10 +14,12 @@ class GenSinus(Node):
         self.amplitude = self.get_parameter('amplitude').value
         self.frequency = self.get_parameter('frequency').value
 
+        self.add_on_set_parameters_callback(self.parameter_callback)
+
         self.publisher_ = self.create_publisher(Float32, 'signal', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.start_time = time.time()
-        #self.get_logger().info(f'Sinusoidal publisher node has been started: amplitude={self.amplitude}, frequency={self.frequency}')
+
         self.get_logger().info(f'Amplitude={self.amplitude}, Frequency={self.frequency}')
 
     def timer_callback(self):
@@ -26,6 +29,16 @@ class GenSinus(Node):
         msg.data = value
         self.publisher_.publish(msg)
         self.get_logger().info(f'Sin(t) = {value:.3f}')
+
+    def parameter_callback(self, params):
+        for param in params:
+            if param.name == 'amplitude' and param.type_ == param.Type.DOUBLE:
+                self.amplitude = param.value
+                self.get_logger().info(f'Updated amplitude to {self.amplitude}')
+            elif param.name == 'frequency' and param.type_ == param.Type.DOUBLE:
+                self.frequency = param.value
+                self.get_logger().info(f'Updated frequency to {self.frequency}')
+        return SetParametersResult(successful=True)
 
 def main(args=None):
     rclpy.init(args=args)   # init ROS2
--- src/my_package/my_package/scaler.py
+++ src/my_package/my_package/scaler.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 import math
 import time
+from rcl_interfaces.msg import SetParametersResult
 
 class ScalerNode(Node):
     def __init__(self):
@@ -13,6 +14,8 @@ class ScalerNode(Node):
         self.scaler_factor = self.get_parameter('scaler_factor').value
         self.offset = self.get_parameter('offset').value
 
+        self.add_on_set_parameters_callback(self.parameter_callback)
+
         self.publisher_ = self.create_publisher(Float32, 'modified_signal',10)
         self.subscription_ = self.create_subscription(
             Float32,
@@ -20,6 +23,7 @@ class ScalerNode(Node):
             self.callback,
             10
         )
+
         self.get_logger().info(f'Scaler node started: scaler_factor={self.scaler_factor}, offset={self.offset}')
 
     def callback(self, msg):
@@ -29,6 +33,16 @@ class ScalerNode(Node):
         self.publisher_.publish(modified_msg)
         self.get_logger().info(f'Received {msg.data:.3f}, -> Scaled {scaled_value:.3f}')
 
+    def parameter_callback(self, params):
+        for param in params:
+            if param.name == 'scalar_factor' and param.type_ == param.Type.DOUBLE:
+                self.scaler_factor = param.value
+                self.get_logger().info(f'Updated scaler_factor to {self.scaler_factor}')
+            elif param.name == 'offset' and param.type_ == param.Type.DOUBLE:
+                self.offset = param.value
+                self.get_logger().info(f'Updated offset to {self.offset}')
+        return SetParametersResult(successful=True)
+    
 def main(args=None):
     rclpy.init(args=args)
     node = ScalerNode()
