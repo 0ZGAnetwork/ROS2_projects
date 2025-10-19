2d9333ee74d0a88cee3b003107426f4c75080672
0ZGA <ozgaapawell@gmail.com>
2025-10-19 13:06:27 +0200

    add launcher for my_package, include 3 nodes- generator.py sclaer.py visualtools.py

--- src/my_package/launch/my.launch.py
+++ src/my_package/launch/my.launch.py
@@ -1,3 +1,24 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='my_package',
+            executable='generator',
+            name='generator_node',
+            output='screen',
+        ),
+        Node(
+            package='my_package',
+            executable='scaler',
+            name='scaler_node',
+            output='screen',
+        ),
+        Node(
+            package='my_package',
+            executable='visualtools',
+            name='visualtools_node',
+            output='screen',
+        ),
+    ])
