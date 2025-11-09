d65df3808ce41ed7105e6159767d2b1de0461743
0ZGA <ozgaapawell@gmail.com>
2025-11-10 00:20:57 +0100

    update sensor2

--- src/robot_monitoring/robot_monitoring/sensor2.py
+++ src/robot_monitoring/robot_monitoring/sensor2.py
@@ -3,28 +3,29 @@ from rclpy.node import Node
 from std_msgs.msg import Bool
 from robot_interfaces.srv import HandleSensorError
 
-class Senstor2Node(Node):
+class Sensor2Node(Node):
     def __init__(self, name):
         super().__init__(name)
-        self.state = True
+        self.state = False
         self.publisher_ = self.create_publisher(Bool, f'/{name}/status', 10)
         self.timer = self.create_timer(1.0, self.publish_state)
-        self.services = self.create_service(HandleSensorError, f'restart_{name}',self.restart_callback)
+        self.srv = self.create_service(HandleSensorError, f'restart_{name}',self.restart_callback)
     
     def publish_state(self):
         msg = Bool()
         msg.data = self.state
         self.publisher_.publish(msg)
+        self.get_logger().info(f'State: {self.state}')
 
     def restart_callback(self, request, response):
         self.get_logger().info(f'Restarting {request.sensor_name}')
-        self.state = False
+        self.state = True
         response.success = True
         return response
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Senstor2Node('sensor2')
+    node = Sensor2Node('sensor2')
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
