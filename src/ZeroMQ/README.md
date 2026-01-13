ba812404656e73ffeb7fa6626226f4a8f149e1dc
0ZGAnetwork <ozgaapawell@gmail.com>
2026-01-13 16:44:58 +0100

    fetch(ZeroMQ): add quaternion and position of robot

--- src/ZeroMQ/README.md
+++ src/ZeroMQ/README.md
@@ -1,1 +1,4 @@
 Two applications communicate with each other using ZeroMQ library. The first application send complex data structure to the second application using serialization. The second application deserialize received data and print the received structure
+
+run :
+# protoc -I=. --python_out=. ./person.proto
--- src/ZeroMQ/ZeroMQ/test_person.py
+++ src/ZeroMQ/ZeroMQ/test_person.py
@@ -5,9 +5,27 @@ p = person_pb2.Person()
 p.name = "Pawel Ozga"
 p.id = 266078
 
+quat = person_pb2.Quaternion()
+quat.x = 1.0
+quat.y = 2.0
+quat.z = 3.0
+quat.w = 5.0
+
+pos = person_pb2.Position()
+pos.x = 10.0
+pos.y = 5.0
+
+robot = person_pb2.Robot()
+robot.position.CopyFrom(pos)
+robot.quaternion.CopyFrom(quat)
+
 #  output
 print("Name:", p.name)
 print("ID:", p.id)
+print("Position:", pos)
+print("Quaternion:", quat)
+print("Robot:", robot)
+print("Robot, only z:", robot.quaternion.z)  # default 0.0
 
 # create binary file
 with open("person.bin", "wb") as f:
@@ -17,5 +35,5 @@ with open("person.bin", "wb") as f:
 p2 = person_pb2.Person()
 with open("person.bin", "rb") as f:
     p2.ParseFromString(f.read())
-
+    
 print("Loaded from file:", p2)
