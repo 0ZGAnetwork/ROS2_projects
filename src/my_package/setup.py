b35f6bdb67a6a31e63beb42f82e304dccd72d854
0ZGAnetwork <ozgaapawell@gmail.com>
2026-01-20 16:18:52 +0100

    chore(workspace)

--- src/my_package/setup.py
+++ src/my_package/setup.py
@@ -10,16 +10,24 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+<<<<<<< HEAD
         ('share/' + package_name + '/config', ['config/generator_params.yaml']),
         ('share/' + package_name + '/config', ['config/scaler_params.yaml']),
         ('share/' + package_name + '/launch',['launch/my.launch.py'])
+=======
+>>>>>>> 287fb1b (INitial commit with working talker package)
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ozga',
     maintainer_email='ozga@todo.todo',
+<<<<<<< HEAD
     description='my package',
     license='MIT',
+=======
+    description='TODO: Package description',
+    license='TODO: License declaration',
+>>>>>>> 287fb1b (INitial commit with working talker package)
     extras_require={
         'test': [
             'pytest',
@@ -27,9 +35,13 @@ setup(
     },
     entry_points={
         'console_scripts': [
+<<<<<<< HEAD
         'generator = my_package.generator:main',
         'visualtools = my_package.VisualTools:main',
         'scaler = my_package.scaler:main',
+=======
+		'talker = my_package.talker:main',
+>>>>>>> 287fb1b (INitial commit with working talker package)
         ],
     },
 )
