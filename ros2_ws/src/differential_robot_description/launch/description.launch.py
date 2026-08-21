5e57dfe83de72fdb588c75da833b9e89c9b3c7f2
Pawel Ozga <ozgaapawell@gmail.com>
2026-08-21 09:15:40 +0200

    feat(Rviz2): add Rviz2 configuration file

---
+++ ros2_ws/src/differential_robot_description/launch/description.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'description.launch.py']),
+        launch_arguments={
+            'urdf_package': 'differential_robot_description',
+            'urdf_package_path': PathJoinSubstitution(['urdf', 'differential_robot.urdf'])}.items()
+    ))
+
+    return ld
