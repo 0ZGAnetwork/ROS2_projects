1687bac681782b8ad0bd5cf9db54bcb77a45f29f
Pawel Ozga <ozgaapawell@gmail.com>
2026-08-22 11:47:28 +0200

    chore(config/): update cmake configuration library

--- ros2_ws/src/differential_robot_description/launch/description.launch.py
+++ ros2_ws/src/differential_robot_description/launch/description.launch.py
@@ -2,16 +2,44 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    ld = LaunchDescription()
 
-    ld.add_action(IncludeLaunchDescription(
-        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'description.launch.py']),
-        launch_arguments={
-            'urdf_package': 'differential_robot_description',
-            'urdf_package_path': PathJoinSubstitution(['urdf', 'differential_robot.urdf.xacro'])}.items()
-    ))
+    return LaunchDescription([
 
-    return ld
+        IncludeLaunchDescription(
+            PathJoinSubstitution([
+                FindPackageShare('urdf_launch'),
+                'launch',
+                'description.launch.py'
+            ]),
+            launch_arguments={
+                'urdf_package': 'differential_robot_description',
+                'urdf_package_path': PathJoinSubstitution([
+                    'urdf',
+                    'differential_robot.urdf.xacro'
+                ])
+            }.items()
+        ),
+
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher'
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=[
+                '-d',
+                PathJoinSubstitution([
+                    FindPackageShare('differential_robot_description'),
+                    'config',
+                    'differential_robot.rviz'
+                ])
+            ]
+        )
+
+    ])
